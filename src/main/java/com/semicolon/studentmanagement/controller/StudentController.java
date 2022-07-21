001078a951008ccf3cceef82443b589207fce61d
Megapreneur <ademolamegbabi@gmail.com>
2022-07-21 23:30:44 +0100

    first commit of Student management application

--- src/main/java/com/semicolon/studentmanagement/controller/StudentController.java
+++ src/main/java/com/semicolon/studentmanagement/controller/StudentController.java
@@ -4,10 +4,9 @@ package com.semicolon.studentmanagement.controller;
 import com.semicolon.studentmanagement.model.data.Student;
 import com.semicolon.studentmanagement.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -20,6 +19,11 @@ public class StudentController {
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
         return "New student has been added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 
 }
--- src/main/java/com/semicolon/studentmanagement/services/StudentService.java
+++ src/main/java/com/semicolon/studentmanagement/services/StudentService.java
@@ -2,6 +2,9 @@ package com.semicolon.studentmanagement.services;
 
 import com.semicolon.studentmanagement.model.data.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
