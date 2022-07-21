d6035def110b73039ec3f3f6c99c51242057a3bc
Megapreneur <ademolamegbabi@gmail.com>
2022-07-21 23:01:55 +0100

    first commit of Student management application

--- src/main/java/com/semicolon/studentmanagement/controller/StudentController.java
+++ src/main/java/com/semicolon/studentmanagement/controller/StudentController.java
@@ -1,8 +1,25 @@
 package com.semicolon.studentmanagement.controller;
 
 
-import org.springframework.stereotype.Controller;
+import com.semicolon.studentmanagement.model.data.Student;
+import com.semicolon.studentmanagement.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student has been added";
+    }
+
 }
--- src/main/java/com/semicolon/studentmanagement/services/StudentServiceImpl.java
+++ src/main/java/com/semicolon/studentmanagement/services/StudentServiceImpl.java
@@ -1,8 +1,18 @@
 package com.semicolon.studentmanagement.services;
 
 
+import com.semicolon.studentmanagement.model.data.Student;
+import com.semicolon.studentmanagement.model.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StudentServiceImpl {
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
